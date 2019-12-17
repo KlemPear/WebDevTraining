5bfb5ada59e51a7bf998c5ba4e00e53298e92273
KlemPear <klem.pearovski@pm.me>
2019-12-16 23:48:51 -0500

    add Todo Project

---
+++ Todo_Project/assets/js/todos.js
@@ -0,0 +1,32 @@
+console.log("Connected!");
+
+// Check off specific Todos by clicking on it
+// select the ul that will always be on the page at the begining
+// and specify inside on() to listen to li inside the ul
+// even if li is created later
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed")
+});
+
+// Click on X to delete Todos
+$("ul").on("click", "span", function(event){
+    // the next $(this) refers to the span, so we use parent()
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove(); // this $(this) refers to the li
+    });
+    event.stopPropagation();
+});
+
+// Creation of new Todos
+$("input[type='text']").keypress(function(event){
+    // check if enter is pressed, key code is 13
+    if (event.which === 13) {
+        // grabbing newx todo text from input
+        var todoText = $(this).val();
+        // clear the input content
+        $(this).val("");
+        // create a new li and add it to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+});
+
